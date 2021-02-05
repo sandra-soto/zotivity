da20ce0419903b84fb4d40fe115650f7d1a38a27
Kelson Lin <kelsonl@uci.edu>
2021-02-05 05:56:12 -0800

    removed some comments

--- lib/pages/SearchPage.dart
+++ lib/pages/SearchPage.dart
@@ -287,31 +287,3 @@ class _SearchPageState extends State<SearchPage> {
     );
   }
 }
-
-// class SearchPage extends StatefulWidget {
-//   @override
-//   _SearchPageState createState() => _SearchPageState();
-// }
-//
-// class _SearchPageState extends State<SearchPage> {
-//   @override
-//   var _num = 0;
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text("Search Page"),
-//         backgroundColor: Colors.blue,
-//       ),
-//       body: Center(
-//         child: RaisedButton(
-//           onPressed: () {
-//             // Navigator.pop(context);
-//             _num = _num + 1;
-//             print(_num);
-//           },
-//           child: Text('Click!'),
-//         ),
-//       ),
-//     );
-//   }
-// }
