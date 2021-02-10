73776c5ab50223ab5015a440e534e02ae850809b
Kelson Lin <kelsonl@uci.edu>
2021-02-09 16:51:15 -0800

    adding to main

---
+++ lib/screens/ActivityPage.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import '../models/Activity.dart';
+
+class ActivityPage extends StatefulWidget {
+  @override
+  Activity activity;
+
+  ActivityPage({this.activity});
+
+  _ActivityPageState createState() => _ActivityPageState();
+}
+
+class _ActivityPageState extends State<ActivityPage> {
+  @override
+  Widget build(BuildContext context) {
+    var screenSize = MediaQuery.of(context).size;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Activity: " + widget.activity.getName()),
+        backgroundColor: Colors.blue,
+      ),
+      body: Container(
+        child: Column(
+          children: [
+            Image(
+              image: NetworkImage(widget.activity.getImgLink()),
+              width: screenSize.width,
+              height: (2 / 5) * screenSize.height,
+            ),
+            Padding(
+              padding: EdgeInsets.all(16.0),
+              child: Text(
+                widget.activity.getName(),
+                style: TextStyle(fontSize: 30),
+                textAlign: TextAlign.center,
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(bottom: 30.0),
+              child: Text("Time required: " +
+                  widget.activity.getTime().toString() +
+                  " minutes"),
+            ),
+            Container(
+              child: SingleChildScrollView(
+                child: Text(
+                  widget.activity.getDescription(),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              width: double.infinity,
+              height: (1 / 5) * screenSize.height,
+            ),
+            Padding(
+              padding: EdgeInsets.only(bottom: 30.0),
+            ),
+            RaisedButton(
+              child: Text("Complete Activity"),
+              onPressed: () {
+                print(
+                    "Activity '" + widget.activity.getName() + "' completed.");
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/screens/CategoryPage.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+import './ActivityPage.dart';
+
+class CategoryPage extends StatefulWidget {
+  @override
+  String categoryName;
+  var categoryList;
+
+  CategoryPage({this.categoryName, this.categoryList});
+
+  _CategoryPageState createState() => _CategoryPageState();
+}
+
+class _CategoryPageState extends State<CategoryPage> {
+  @override
+  Widget build(BuildContext context) {
+    // var activityList = [a,b,c];
+    var screenSize = MediaQuery.of(context).size;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Category: " + widget.categoryName),
+        backgroundColor: Colors.blue,
+      ),
+      body: ListView.builder(
+        padding: const EdgeInsets.all(8.0),
+        itemCount: widget.categoryList.length,
+        itemBuilder: (BuildContext context, int index) {
+          return Container(
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(top: 10.0),
+                  child: Image(
+                    image:
+                        NetworkImage(widget.categoryList[index].getImgLink()),
+                    width: screenSize.width,
+                    height: 200,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 10.0, bottom: 60.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(left: 15.0),
+                        child: Column(
+                          children: [
+                            Text(
+                              widget.categoryList[index].getName(),
+                              style: TextStyle(fontSize: 28),
+                              textAlign: TextAlign.left,
+                            ),
+                            Text(widget.categoryList[index]
+                                    .getTime()
+                                    .toString() +
+                                " minutes"),
+                          ],
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(right: 15.0),
+                        child: RaisedButton(
+                          child: Text("Let's Do It!"),
+                          onPressed: () {
+                            Navigator.push(
+                                context,
+                                new MaterialPageRoute(
+                                    builder: (__) => new ActivityPage(
+                                        activity: widget.categoryList[index])));
+                          },
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
