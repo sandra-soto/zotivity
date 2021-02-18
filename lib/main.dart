5dc3eeabb6f9be401d9b1e3c3a933ba5e5140e75
jiaaaeeeee <798275175@qq.com>
2021-02-17 19:18:34 -0800

    add weather detail and location privacy request screen

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:weather/weather.dart';
-import 'package:http/http.dart' as http;
 
 void main() {
   runApp(MaterialApp(
@@ -19,44 +18,33 @@ class _AppState extends State<App> {
 
   double lat;
   double lon;
+  Weather weatherReport;
 
-  Future<Position> getPosition() async {
+  Future<Position> getCurrentCord() async {
     Position position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
     return position;
   }
 
-  getCurrentLat() async {
 
-    final position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
-    return position.latitude;
-
-  }
-
-  getCurrentLon() async {
-
-    final position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
-    return position.longitude;
-
-  }
-
-
-  Future<String> getData(double latitude, double longitude) async {
+  Future<Weather> getData(double latitude, double longitude) async {
     WeatherFactory wf = WeatherFactory('3d45c6ea75793ed583d1664d77b8bb50');
     Weather w = await wf.currentWeatherByLocation(latitude, longitude);
     print(w);
-    //return parsed['list'][0]['weather'][0]['description'];
+    return w;
   }
 
   void initState() {
     super.initState();
-    getPosition().then((position) {
-      setState(() {
-        lon = position.longitude;
-        lat = position.latitude;
+    getCurrentCord().then((position) {
+      getData(position.latitude, position.longitude).then((weather) {
+        setState(() {
+          lon = position.longitude;
+          lat = position.latitude;
+          weatherReport = weather;
+        });
       });
     });
   }
-
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
@@ -74,7 +62,7 @@ class _AppState extends State<App> {
                     Icon(
                         Icons.location_on, size:100, color:Colors.grey,
                     ),
-                    //Text('$_weather'),
+                    Text('$weatherReport'),
                     FlatButton(
                         onPressed: () {
                           getData(lat,lon);
